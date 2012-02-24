ab291aa7e06cf2acbcbadb73b6e9b32aaebf6028
valeron <t-gult_23@hotmail.com>
2012-02-24 23:15:28 +0200

    ilk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Borcumborc3::Application.config.secret_token = '287220c6507bc36c6c2eae3937dbecd0092e4cc4fc4f2960a552ccf2437bbf061081e52940439280880553d1a2f9ad57e1842322269ef5f07006bb0b7c3b6c4c'
